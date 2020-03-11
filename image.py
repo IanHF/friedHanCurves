26d8fde5c7c9397a86118bcb627b90b9981db0bf
Ian Fried <ifried00@stuy.edu>
2020-03-11 09:30:31 -0400

    done...i think

--- image.py
+++ image.py
@@ -1,10 +1,17 @@
 #SPECIAL THANKS TO IAN WILLIAMS FOR HELPING ME WITH SOME DEBUGGING, PRAISE PROGRAMMER JESUS, PEACE BE UPON HIM
 #<3 IW
 import math
-
+import os
 #multiply m1 by m2, modifying m2 to be the product
 #m1 * m2 -> m2
-def matrix_mult( m1, m2 ):
+def matrix_mult( m1, m2 ): #IW Fixed your matrix multiplication function to work with non 4xn matrices
+    temp = new_matrix(len(m1), len(m2[0]))
+    for row in range(len(temp)):
+        for col in range(len(temp[0])):
+            for i in range(len(m1[0])):
+                temp[row][col] += m1[row][i] * m2[i][col]
+    m2 = temp
+    return temp
 
     point = 0
     for row in m2:
@@ -28,6 +35,12 @@ def ident( matrix ):
 
 def new_matrix(rows = 4, cols = 4):
     m = []
+    for r in range(rows):
+        m.append([])
+        for c in range(cols):
+            m[r].append(0)
+    return m
+    # IW The bottom code seems to be reversed. It should be like above
     for c in range( cols ):
         m.append( [] )
         for r in range( rows ):
@@ -82,8 +95,8 @@ def make_rotZ( theta ):
     return t
 
 def circle_point(x, y, theta, r):
-    xn = x + (r * math.cos(theta))
-    yn = y + (r * math.sin(theta))
+    xn = x + (r * math.cos(math.radians(theta)))
+    yn = y + (r * math.sin(math.radians(theta)))
     return [xn, yn]
 
 class picture:
@@ -98,7 +111,7 @@ class picture:
     def plot(self, x, y, color):
         if x < 0 or y < 0 or x >= self.width or y >= self.height:#out of bounds
             return
-        self.pixels[self.height - y - 1][x] = color #Flip for humans
+        self.pixels[int(self.height - y - 1)][int(x)] = color #Flip for humans
 
     def draw_line(self, x0, y0, x1, y1, color ):
         #swap points if going right -> left
@@ -190,6 +203,7 @@ class picture:
         self.edge_matrix[1].append(y)
         self.edge_matrix[2].append(z)
         self.edge_matrix[3].append(1)
+        #print x, y, z
 
     def add_edge(self, x0, y0, z0, x1, y1, z1):
         self.add_3d_point(x0,y0,z0)
@@ -197,53 +211,82 @@ class picture:
 
 
     def add_circle(self, cx, cy, cz, r, step ):
+
         centx = cx
         centy = cy
         theta = 0
-        while(theta != 360):
-            secondpoints = circle_point(centx, centy, step, r)
-            self.add_edge(centx, centy, 1, secondpoints[0], secondpoints[1], 1)
-            centx = secondpoints[0]
-            centy = secondpoints[1]
+        last_pos = circle_point(centx, centy, 0, r)
+        theta += step
+        while(theta <= 360 + step):
+            secondpoints = circle_point(centx, centy, theta, r)
+            self.add_edge(last_pos[0], last_pos[1], 1, secondpoints[0], secondpoints[1], 1)
+            #centx = secondpoints[0]
+            #centy = secondpoints[1]
+            last_pos = secondpoints
             theta += step
 
 
     def add_curve(self, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type ):
+        t = 0
         if curve_type == "bezier":
-            xa = (-1 * x0) + (3 * x1) - (3 * x2) + (x * 3)
+            # I can't remember how this works, so i reimplemented it IW
+            """
+            xa = (-1 * x0) + (3 * x1) - (3 * x2) + (x3 * 3)
             xb = (3 * x0) - (6 * x1) + (3 * x2)
             xc = (-3 * x0) + (3 * x1)
-            xd = p0
-            for(t = 0, t < 1, t += step):
-                plotx = xa * t * t * t + xb * t * t + xc * t + xd
-	            ploty = ya * t * t * t + yb * t * t + yc * t + yd
+            xd = x0
+            """
+            #for(t = 0, t < 1, t += step): IW incorrect syntax
 
-	            dt = t + step
+            while t <= 1:
+                d = [pow(1 - t, 3), 3 * t * pow(1 - t, 2), 3 * pow(t, 2) * (1 - t), pow(t, 3)]
+                x = (x0 * d[0]) + (x1 * d[1]) + (x2 * d[2]) + (x3 * d[3])
+                y = (y0 * d[0]) + (y1 * d[1]) + (y2 * d[2]) + (y3 * d[3])
+                self.add_3d_point(x, y, 0)
+                #plotx = xa * t * t * t + xb * t * t + xc * t + xd
+	        #ploty = ya * t * t * t + yb * t * t + yc * t + yd
 
-	            newplotx = xa * dt * dt * dt + xb * dt * dt + xc * dt + xd
-	            newploty = ya * dt * dt * dt + yb * dt * dt + yc * dt + yd
+	        #dt = t + step
 
-	            self.add_edge(plotx, ploty, 1, newplotx, newploty, 1)
+	        #newplotx = xa * dt * dt * dt + xb * dt * dt + xc * dt + xd
+	        #newploty = ya * dt * dt * dt + yb * dt * dt + yc * dt + yd
+
+	        #self.add_edge(plotx, ploty, 1, newplotx, newploty, 1)
+                t += step
         else:
             m = make_hermite()
             pmatrix = [[x0, y0, 1], [x1, y1, 1], [x2, y2, 1], [x3, y3, 1]]
-            for(t = 0, t < 1, t += step):
-                tmatrix = [t * t * t, t * t, t, 1]
-                self.add_3d_point(matrix_mult(matrix_mult(tmatrix, m), pmatrix))
+            #for(t = 0, t < 1, t += step):
+            while t <= 1:
+                
+                tmatrix = [[t * t * t, t * t, t, 1]]
+                #print "bef", m
+                #matrix_mult(tmatrix, m)
+                #print "aft", m
+                a = matrix_mult(matrix_mult(tmatrix, m), pmatrix)
+                #print a
+                
+                #self.add_3d_point(matrix_mult(matrix_mult(tmatrix, m), pmatrix))
+                self.add_3d_point(a[0][0], a[0][1], a[0][2])
+                
+                t += step
 
 
-    def draw_lines( matrix, screen, color ):
+    def draw_lines(self):#,  matrix, screen, color ):
+        #self.display_edge_matrix()
+        matrix = self.edge_matrix
+        #display_matrix(matrix)
         if len(matrix) < 2:
             print('Need at least 2 points to draw')
             return
 
         point = 0
-        while point < len(matrix) - 1:
-            draw_line( int(matrix[point][0]),
-                int(matrix[point][1]),
-                int(matrix[point+1][0]),
-                int(matrix[point+1][1]),
-                screen, color)
+        while point < len(matrix[0]) - 1:
+            self.draw_line( int(matrix[0][point]),
+                int(matrix[1][point]),
+                int(matrix[0][point+1]),
+                int(matrix[1][point+1]),
+                       [255, 255, 255])
             point+= 2
 
 def print_pic_ascii(g):
@@ -288,7 +331,7 @@ def display_matrix(x):
 
 ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'bezier', 'hermite', 'circle' ]
 
-def parse_file( fname, edges, transform, screen, color ):
+def parse_file( fname, transform, screen, color ):
 
     f = open(fname)
     lines = f.readlines()
@@ -305,7 +348,7 @@ def parse_file( fname, edges, transform, screen, color ):
         if line == 'line':
             #print 'LINE\t' + str(args)
 
-            add_edge( edges,
+            screen.add_edge( edges,
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]) )
 
@@ -315,12 +358,12 @@ def parse_file( fname, edges, transform, screen, color ):
         elif line == 'bezier':
             screen.add_curve(float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]),
-                      float(args[6]), float(args[7]), 0.02, "bezier")
+                      float(args[6]), float(args[7]), 0.005, "bezier")
 
         elif line == 'hermite':
             screen.add_curve(float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]),
-                      float(args[6]), float(args[7]), 0.02, "hermite")
+                      float(args[6]), float(args[7]), 0.005, "hermite")
 
         elif line == 'scale':
             #print 'SCALE\t' + str(args)
@@ -351,13 +394,17 @@ def parse_file( fname, edges, transform, screen, color ):
             matrix_mult( transform, edges )
 
         elif line == 'display' or line == 'save':
-            clear_screen(screen)
-            draw_lines(edges, screen, color)
-
+            #clear_screen(screen)
+            screen.draw_lines()#edges, screen, color)
+            #print print_pic_ascii(screen)
+            ppm_save_ascii(screen)
             if line == 'display':
-                display(screen)
+                #save_extension(screen, args[0])
+                #display(screen)
+                os.system('display *.ppm')
             else:
-                save_extension(screen, args[0])
+                pass
+                #save_extension(screen, args[0])
 
         c+= 1
 
@@ -366,10 +413,13 @@ def parse_file( fname, edges, transform, screen, color ):
 #draw_line(self, x0, y0, x1, y1, color )
 #add_edge(self, x0, y0, z0, x1, y1, z1)
 #draw_edges(self, color)
-parse_file( 'script', edges, transform, screen, color )
+screen = picture('image', 500, 500)
+transform = [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+color = [255, 255, 255]
+parse_file( 'script', transform, screen, color )
 
-n = picture('image', 500, 500)
+#n = picture('image', 500, 500)
 
-n.draw_edges([255, 255, 255])
+#n.draw_edges([255, 255, 255])
 
-ppm_save_ascii(n)
+#ppm_save_ascii(n)
